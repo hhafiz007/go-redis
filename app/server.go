dc22c21f09029bfb9f925338cb716cc959be119e
Hamza Hafiz <hamzahafiz@Hamzas-MBP.lan>
2024-01-25 00:17:09 -0500

    added support for multiple clients

--- app/server.go
+++ app/server.go
@@ -12,16 +12,22 @@ func main() {
 	serverAlive := []byte("+PONG\r\n")
 
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	fmt.Println("Listening to connections", l)
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
 
-	_, err = conn.Write(serverAlive)
+	for i := 0; i < 2; i++ {
+
+		conn, _ := l.Accept()
+		_, err = conn.Write(serverAlive)
+
+	}
 
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
 }
