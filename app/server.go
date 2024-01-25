c648ea366f7f86449b8bc83ff4cfe63e1fec90d0
Hamza Hafiz <hamzahafiz@Hamzas-MBP.lan>
2024-01-24 23:22:18 -0500

    returning pong to server

--- app/server.go
+++ app/server.go
@@ -9,12 +9,17 @@ import (
 
 func main() {
 
+	serverAlive := []byte("+PONG\r\n")
+
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
+
+	_, err = conn.Write(serverAlive)
+
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
