9644094bc1bd3a45a81629ce9113bbc8f1501da7
Hamza Hafiz <hamzahafiz@Hamzas-MBP.lan>
2024-01-27 23:57:48 -0500

    applied parser

--- app/parser.go
+++ app/parser.go
@@ -121,7 +121,7 @@ func parseBulkString(stream *bufio.Reader) (RedisMessage, error) {
 		bytes: data[:len(data)-2],
 	}
 
-	fmt.Println("Bulak String is", myMsg, string(myMsg.bytes))
+	fmt.Println("Bulak Striang is", myMsg, string(myMsg.bytes))
 
 	return myMsg, nil
 
