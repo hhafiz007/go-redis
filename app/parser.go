2898888a172feb77a1c52c3bca053505ca3fd1be
Hamza Hafiz <hamzahafiz@Hamzas-MBP.lan>
2024-01-27 23:59:31 -0500

    applied parser

--- app/parser.go
+++ app/parser.go
@@ -107,7 +107,7 @@ func parseBulkString(stream *bufio.Reader) (RedisMessage, error) {
 
 	length, _ := strconv.Atoi(string(bytes[0]))
 	data := make([]byte, length+2)
-	fmt.Println("The length aof data is ", length, bytes)
+	fmt.Println("The length of data is ", length, bytes)
 
 	_, err = stream.Read(data)
 
